6c1df28563a8a599a372d43fde0c8ba6164965b7
Ali Pirzadeh <ali2021pirzadeh@gmail.com>
2025-04-30 00:27:23 +0330

    style sidebar

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -5,8 +5,8 @@ import image from "./../assets/man.jpg";
 function SideBar() {
   return (
     <>
-      <div className="w-70 bg-white rounded-md pb-5 mt-4">
-        <img src={image} className="w-70 rounded" />
+      <div className="w-full bg-white rounded-md pb-5 mt-4">
+        <img src={image} className="w-full rounded" />
         <div className="text-center mt-1">
           <h2 className="text-2xl mb-2 ">Personal Advisor</h2>
           <p className="text-sm md:text-md ">
@@ -14,7 +14,7 @@ function SideBar() {
             we're ready to help!
           </p>
         </div>
-        <div className="mx-2">
+        <div className="mx-2 pt-4">
           <button className="border-1 rounded-md px-3 py-2 hover:bg-black hover:text-white">Chat new</button>
         </div>
       </div>
