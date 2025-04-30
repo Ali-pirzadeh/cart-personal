452edd8f8d8b27632e7261a1e4b85f8a9ab88e08
Ali Pirzadeh <ali2021pirzadeh@gmail.com>
2025-04-30 22:52:44 +0330

    main edit

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import image from "./../assets/shop-sabad.jpg";
 import { FaInstagram, FaCircle } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa";import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { BiLike } from "react-icons/bi";
+import { MdOutlineCheckCircle } from "react-icons/md";
+import { TfiWrite } from "react-icons/tfi";
+import { IoLogoInstagram } from "react-icons/io5";
+
+
 
 function Main() {
+  const [options, setOptions] = useState([
+    "100 instagram",
+    "200 instagram",
+    "300 instagram",
+    "500 instagram",
+  ]);
+  const [selected, setSelected] = useState(options[0]);
+
+  const [instagram, setInstagram] = useState("")
+  
+  const inputHandeler = (e) => {
+    const value = (e.target.value)
+    setInstagram(value)
+    console.log(value);
+  }
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if (value === selected) return;
+
+    const newOptions = options.filter((opt) => opt !== value);
+    newOptions.unshift(value); 
+    setOptions(newOptions);
+    setSelected(value);
+  };
+  
   return (
-    <div className="bg-white rounded-md mt-4 w-full">
+    <div className="bg-white rounded-md mt-4 w-full mb-4">
       <div className="w-full">
         <img
           src={image}
@@ -36,6 +67,55 @@ function Main() {
         <p className="text-sm pl-2">
           Service Active • Last checked on: April 29, 2025
         </p>
+      </div>
+      <div className="md:flex items-center pt-3 mx-4">
+        <div className="flex items-center mr-2">
+          <MdOutlineCheckCircle className="text-xl mr-1 text-gray-500" />
+          <p> Premium Quality</p>
+        </div>
+        <div className="flex items-center mr-2">
+          <MdOutlineCheckCircle className="text-xl mr-1 text-gray-500" />
+          <p> Premium Quality</p>
+        </div>
+        <div className="flex items-center mr-2">
+          <MdOutlineCheckCircle className="text-xl mr-1 text-gray-500" />
+          <p> Premium Quality</p>
+        </div>
+        <div className="flex items-center mr-2">
+          <MdOutlineCheckCircle className="text-xl mr-1 text-gray-500" />
+          <p> Premium Quality</p>
+        </div>
+        <div className="flex items-center">
+          <MdOutlineCheckCircle className="text-xl mr-1 text-gray-500" />
+          <p> Premium Quality</p>
+        </div>
+      </div>
+      <div className="flex items-center pt-3 mx-4">
+        <TfiWrite className="text-xl mr-2" />
+        <h3 className="text-md md:text-2xl font-bold">Select your Package:</h3>
+      </div>
+      <div className="flex items-center pt-3 mx-4">
+        <select
+          value={selected}
+          onChange={handleChange}
+          className="w-full p-3 border rounded-lg shadow text-gray-800"
+        >
+          {options.map((opt, index) => (
+            <option key={index} value={opt}>
+              {opt}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex items-center mt-3 mx-4 border-1 border-gray-400 rounded-md">
+        <IoLogoInstagram className="mr-2 text-xl mx-2" />
+        <input
+          type="text"
+          placeholder="instagram"
+          value={instagram}
+          onChange={inputHandeler}
+          className="flex-1 py-3 bg-transparent border-none outline-none focus:ring-0 focus:border-none"
+        />
       </div>
     </div>
   );
